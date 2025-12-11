3351a7848a4e300dccdd10786689099b9420665d
lewis1190 <lewiskos@hotmail.co.uk>
2025-12-11 19:29:37 +0000

    Added Comment model and registered it in admin; updated Post model with ordering and string representation

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 
 # ERD table available at: readme_docs_assets\comment_erd.png
 
@@ -28,3 +34,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} | by {self.author}"
