3580cd6e564d97f0f014088c25ac2953e1d550a8
lewis1190 <lewiskos@hotmail.co.uk>
2025-12-11 19:30:48 +0000

    Refactored SECRET_KEY and DEBUG settings for improved environment handling

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY') if os.path.isfile('env.py') else False
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG') if os.path.isfile('env.py') else False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
