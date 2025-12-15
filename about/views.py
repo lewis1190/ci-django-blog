6d5f83b4fb6607948fed765c604e67a3dad5b151
lewis1190 <lewiskos@hotmail.co.uk>
2025-12-15 19:38:09 +0000

    Added and implemented collaboration request. Form, views and associated models.

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import About
+from django.contrib import messages
+from .forms import CollaborateRequestForm
 
 # Create your views here.
 
@@ -13,4 +15,20 @@ def about_me(request):
     :template:`about/about.html`
     """
     about = About.objects.all().order_by("-updated_on").first()
-    return render(request, "about/about.html", {"about": about})
+
+    if request.method == "POST":
+        collaborate_form = CollaborateRequestForm(request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save(commit=True)
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+
+    collaborate_form = CollaborateRequestForm()
+    return render(
+        request, "about/about.html",
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        })
