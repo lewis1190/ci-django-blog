d5fb373ed4bdb02fdd85608cab0be86d69ab0c3c
lewis1190 <lewiskos@hotmail.co.uk>
2025-12-18 23:41:20 +0000

    Add unit tests for About and Blog views, including collaboration and comment submissions

---
+++ about/test_views.py
@@ -0,0 +1,33 @@
+from django.urls import reverse
+from django.test import TestCase
+from .forms import CollaborateForm
+from .models import About
+
+
+class TestAboutViews(TestCase):
+
+    def setUp(self):
+        """Creates about me content"""
+        self.about = About(title="About title", content="About content")
+        self.about.save()
+
+    def test_render_about_page_with_collaborate_form(self):
+        """Verifies get request for about me containing a collaboration form"""
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"About title", response.content)
+        self.assertIn(b"About content", response.content)
+        self.assertIsInstance(
+            response.context['collaborate_form'], CollaborateForm)
+
+    def test_successful_collaboration_request_submission(self):
+        """Test for a user requesting a collaboration"""
+        post_data = {
+            'name': 'test name',
+            'email': 'test@email.com',
+            'message': 'test message'
+        }
+        response = self.client.post(reverse('about'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'Collaboration request received! I endeavour to respond within 2 working days.', response.content)
